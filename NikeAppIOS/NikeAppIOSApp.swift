151305dce168fe904f7dca9e29290334a248793e
Daniil Ternavsky <topkek555228@gmail.com>
2025-11-10 12:57:57 +0300

    Initial Commit

---
+++ NikeAppIOS/NikeAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeAppIOSApp.swift
+//  NikeAppIOS
+//
+//  Created by Лариса on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NikeAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
